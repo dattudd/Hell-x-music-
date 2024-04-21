0b1f0a2356f62b2c1697e237b809b82b265d550a
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-04-21 12:56:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "20798317"))
 API_HASH = getenv("API_HASH", "82a94d416e05ca5cc3bc04da8494d7ca")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7151436558:AAEFAkvzcpML8lw5-BzCwnHKTCJWOncstqE")
+BOT_TOKEN = getenv("BOT_TOKEN", "7151436558:AAEbAivKjdHySDF1zrrXaZIUXn523UJ_vQ0")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
